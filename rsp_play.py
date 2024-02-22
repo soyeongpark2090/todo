71a26eed4620d4c8cec93b4dc5437c6d987ab7b2
soyeongpark2090 <syp4546@gmail.com>
2024-02-22 22:58:09 +0900

    input 때마다 다시 유효성 검사하도록

--- rsp_play.py
+++ rsp_play.py
@@ -1,19 +1,23 @@
 import random
-random_computer = random.randiant(1, 3)
-get = input('가위, 바위, 보 중 하나를 선택하세요: ')
-range = ['가위', '바위', '보']
+rsp = ['가위', '바위', '보']
+computer_random = random.choice(rsp)
 
 while True:
-    if get != ('가위' or '바위' or '보'):
+    user_choice = input('가위, 바위, 보 중 하나를 선택하세요: ')
+    if user_choice not in rsp:
         print('유효한 입력이 아닙니다')
-        break
+        continue
     else:
         break
-    
-get = input('가위, 바위, 보 중 하나를 선택하세요: ')
 
-if get = '가위'
+print(f'사용자: {user_choice}, 컴퓨터: {computer_random}')
 
-user_choice = int(get)
 while True:
-    if
+    if user_choice == computer_random:
+        print('비겼습니다!')
+    elif (user_choice == rsp[0] and computer_random == rsp[2]) or (user_choice == rsp[1] and computer_random == rsp[0]) or (user_choice == rsp[2] and computer_random == rsp[1]):
+        print('사용자 승리!')
+    else:
+        print('컴퓨터 승리!')
+        break
+print(input('다시 하시겠습니까? (y/n): '))
