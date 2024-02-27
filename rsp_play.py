2e2ec91535ae9792a1334bfcba34d29ca8615661
soyeongpark2090 <syp4546@gmail.com>
2024-02-27 14:43:35 +0900

    rsp_play.py 함수 수정

--- rsp_play.py
+++ rsp_play.py
@@ -4,7 +4,7 @@ import random
 def check_winner():
     draw_cnt = 0
     win_cnt = 0
-    lose_cnt = 0
+    lost_cnt = 0
 
     rsp = ['가위', '바위', '보']
     computer_pick = random.choice(rsp)
@@ -15,54 +15,29 @@ def check_winner():
             print('유효한 입력이 아닙니다')
             continue
 
-        if user_pick == '가위':
-            if computer_pick == '가위':
-                print('비겼어요!')
-                draw_cnt += 1
-                break
-            elif computer_pick == '바위':
-                print('컴퓨터 승리!')
-                lose_cnt += 1
-            elif computer_pick == '보':
-                print('사용자 승리!')
-                win_cnt += 1
-            print(f'사용자: {user_pick}, 컴퓨터: {computer_pick}')
-        elif user_pick == '바위':
-            if computer_pick == '가위':
-                print('사용자 승리!')
-                win_cnt += 1
-            elif computer_pick == '바위':
-                print('비겼어요!')
-                draw_cnt += 1
-                break
-            elif computer_pick == '보':
-                print('컴퓨터 승리!')
-                lose_cnt += 1
-            print(f'사용자: {user_pick}, 컴퓨터: {computer_pick}')
-        elif user_pick == '보':
-            if computer_pick == '가위':
-                print('컴퓨터 승리!')
-                lose_cnt += 1
-            elif computer_pick == '바위':
-                print('사용자 승리!')
-                win_cnt += 1
-            elif computer_pick == '보':
-                print('비겼어요!')
-                draw_cnt += 1
-                break
-            print(f'사용자: {user_pick}, 컴퓨터: {computer_pick}')
-    return draw_cnt, win_cnt, lose_cnt
+        if user_pick == computer_pick:
+            print('비겼어요!')
+            draw_cnt += 1
+        elif (user_pick=='가위' and computer_pick=='보') or (user_pick=='바위' and computer_pick=='가위') or (user_pick=='보' and computer_pick=='바위'):
+            print('사용자 승리!')
+            win_cnt += 1
+            break
+        else:
+            print('컴퓨터 승리!')
+            lost_cnt += 1
+        print(f'사용자: {user_pick}, 컴퓨터: {computer_pick}')
+    return win_cnt, draw_cnt, lost_cnt
 
 
 while True:
     # check_winner() 삭제
-    draw_cnt, win_cnt, lose_cnt = check_winner()
+    draw_cnt, win_cnt, lost_cnt = check_winner()
     restart = input('다시 시작하시겠습니까? (y/n): ')
     if restart == 'y':
         print('게임을 재시작합니다')
     elif restart == 'n':
         print('게임을 종료합니다')
-        print(f'승:{win_cnt}, 무:{draw_cnt}, 패:{lose_cnt}')
+        print(f'승:{win_cnt}, 무:{draw_cnt}, 패:{lost_cnt}')
         break
     else:
         break
